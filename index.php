7896428a6ab630b7f06fa5f89d76e7dabc9cc56a
Sir Louis <me@sirlouis.io>
2020-04-19 23:28:29 +0100

    bug fix

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $page_name = "faqs";
     <link rel="stylesheet" href="<?php echo \html_style("base.css"); ?>">
   </head>
   <body>
-    <?php \setup_page('faqs','base', true, PRJ_HEADER_HEIGHT); ?>
+    <?php \TymFrontiers\Helper\setup_page('faqs','base', true, PRJ_HEADER_HEIGHT); ?>
     <?php include PRJ_INC_HEADER; ?>
     <section id="main-content">
       <div class="view-space">
